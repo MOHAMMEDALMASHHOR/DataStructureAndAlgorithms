ade7fcd0ec46e9236e89c6b00c3e387078be8f06
MOHAMMEDALMASHHOR <u211118091@samsun.edu.tr>
2024-03-27 18:19:37 +0300

    asdf

--- DataStractureInC#/BinarySearch/BinarySearch.cs
+++ DataStractureInC#/BinarySearch/BinarySearch.cs
@@ -1,2 +1,18 @@
 //First of all we have to create the algorithm for the Binary search 
-//Which is depending on 
+//Which is depending on sorting the elemants and then find the middle element and then compare the middle element with the target element
+//If the target element is greater than the middle element then we have to search the right side of the middle element
+//If the target element is less than the middle element then we have to search the left side of the middle element
+//If the target element is equal to the middle element then we have to return the index of the middle element
+//If the target element is not found then we have to return -1
+//The time complexity of the Binary search is O(log n)
+//The space complexity of the Binary search is O(1)
+//The Binary search is the most efficient searching algorithm
+//The Binary search is used to search the element in the sorted array
+//The Binary search is used to search the element in the sorted list
+//The Binary search is used to search the element in the sorted matrix
+//The Binary search is used to search the element in the sorted string
+//The Binary search is used to search the element in the sorted tree
+public class BinarySearch
+{
+    
+}
