9f16c06ecd8d70286dd01be7a86ddba8158db6bb
MOHAMMEDALMASHHOR <u211118091@samsun.edu.tr>
2024-04-01 20:21:41 +0300

    sfd

--- DataStractureInC#/BinarySearch/Program.cs
+++ DataStractureInC#/BinarySearch/Program.cs
@@ -1,43 +1,43 @@
 ﻿Console.Write("Enter a number: ");
 
-        // Read the input from the user
-        string userInput = Console.ReadLine();
+// Read the input from the user
+string userInput = Console.ReadLine();
 
-        int number;
+int number;
 
-        // Try to parse the input string to an integer
-        if (int.TryParse(userInput, out number))
-        {
-            // Conversion successful
-            Console.WriteLine("You entered: " + number);
-        }
-        else
-        {
-            // Conversion failed
-            Console.WriteLine("Invalid input. Please enter a valid integer.");
-        }
+// Try to parse the input string to an integer
+if (int.TryParse(userInput, out number))
+{
+    // Conversion successful
+    Console.WriteLine("You entered: " + number);
+}
+else
+{
+    // Conversion failed
+    Console.WriteLine("Invalid input. Please enter a valid integer.");
+}
 int[] arr = new int[number];
-        for (int i = 0; i < number; i++)
-        {
-            System.Console.WriteLine("Enter the value for index " + i+ ": ");
-            var newValue = int.Parse(Console.ReadLine());   
-            arr[i] =newValue ;
-        }
-        BinarySearch bs = new BinarySearch();
-        int target = 5;
-        int result = bs.Search(arr, target);
-        if (result != -1)
-        {
-            Console.WriteLine("The element is found at index: " + result);
-        }
-        else
-        {
-            Console.WriteLine("The element is not found!!!");
-        }
+for (int i = 0; i < number; i++)
+{
+    System.Console.WriteLine("Enter the value for index " + i + ": ");
+    var newValue = int.Parse(Console.ReadLine());
+    arr[i] = newValue;
+}
+BinarySearch bs = new BinarySearch();
+int target = 5;
+int result = bs.Search(arr, target);
+if (result != -1)
+{
+    Console.WriteLine("The element is found at index: " + result);
+}
+else
+{
+    Console.WriteLine("The element is not found!!!");
+}
 string array1 = "{";
 foreach (var item in arr)
 {
-    array1 += item.ToString() + ",";    
+    array1 += item.ToString() + ",";
 }
 array1 += "}";
 System.Console.WriteLine("The array is: " + array1);
